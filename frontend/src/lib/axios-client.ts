37b5b04b0e4e27fabbf4e3b8506b2698af574411
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-04-06 19:26:11 +0600

    refactor: Change condition in axios client

--- frontend/src/lib/axios-client.ts
+++ frontend/src/lib/axios-client.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const getBaseURL = () => {
+  if (import.meta.env.VITE_API_URL) return `${import.meta.env.VITE_API_URL}/api/v1`;
+  return "/api/v1";
+};
+
 export const API = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? `${import.meta.env.VITE_API_URL}/api/v1`
-      : "/api/v1",
+  baseURL: getBaseURL(),
   withCredentials: true,
 });
